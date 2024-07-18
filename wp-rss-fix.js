afcab535d9bc1425fe0c7e4ed82cbccbb1f24919
Robert Altman <rjaltman3@gmail.com>
2024-07-18 14:05:29 -0500

    Add regular expression to check for HTML elements before rewriting the link text

--- wp-rss-fix.js
+++ wp-rss-fix.js
@@ -1,5 +1,7 @@
 // Script to fix WordPress special character 
 // rendering in RSS feeds.
+// Define a regular expression pattern to check for HTML elements
+const re = /<[a-zA-Z]+>/;
 // Wait until the DOM has loaded
 document.addEventListener('DOMContentLoaded', (e) => {
     // Identify the RSS feed titles on the page
@@ -8,9 +10,12 @@ document.addEventListener('DOMContentLoaded', (e) => {
     for (let i = 0; i < rssTitles.length; i++) {
         // Find the article title <a> element (we do not want to disturb the URL)
         const articleLink = rssTitles[i].querySelector('a');
-        // Replace the inner HTML of the article link 
-        // element with the plain text string that WordPress
-        // is rendering as HTML when decoding special characters
-        articleLink.innerHTML = rssTitles[i].textContent;
+        // If the text content of the article link 
+        // contains HTML elements,replace the inner HTML 
+        // of the article link element with the plain text string 
+        // that WordPress is rendering as HTML when decoding special characters
+        if (re.test(articleLink.textContent)) {
+            articleLink.innerHTML = rssTitles[i].textContent;
+        }
     }
 });
